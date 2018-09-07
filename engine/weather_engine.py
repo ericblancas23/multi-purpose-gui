1461c54d615408e9219d72f0f5508425a0b77a2e
EricB <ericblancas23@gmail.com>
2018-09-06 18:56:53 -0700

    initilized and created first engine with py

---
+++ engine/weather_engine.py
@@ -0,0 +1,12 @@
+from rapidconnect import RapidConnect
+import sys`
+
+def weather(city):
+    rapid = RapidConnect("default-application_59bbda2be4b0b0cacf7c7995",
+                     "db4efe57-20a5-4f2d-966c-b292a14a8695")
+
+    result = rapid.call('YahooWeatherAPI', 'getWeatherForecast', {'location': city})
+
+
+    desc = result["query"]["results"]["channel"]["item"]["condition"]["temp"]
+    temp = result["query"]["results"]["channel"]["item"]["condition"]["temp"]
