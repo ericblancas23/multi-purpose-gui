e7716c2b46dce4a4cbaaa8e0ffc250978fd18757
EricB <ericblancas23@gmail.com>
2018-09-09 23:04:56 -0700

    finished face.py

--- engine/weather_engine.py
+++ engine/weather_engine.py
@@ -1,15 +1,16 @@
 from rapidconnect import RapidConnect
 import sys
 
-def weather(city):
-    rapid = RapidConnect("default-application_59bbda2be4b0b0cacf7c7995",
+city = sys.argv[1]
+
+rapid = RapidConnect("default-application_59bbda2be4b0b0cacf7c7995",
                      "db4efe57-20a5-4f2d-966c-b292a14a8695")
 
-    result = rapid.call('YahooWeatherAPI', 'getWeatherForecast', {'location': city})
+result = rapid.call('YahooWeatherAPI', 'getWeatherForecast', {'location': city})
 
 
-    desc = result["query"]["results"]["channel"]["item"]["condition"]["temp"]
-    temp = result["query"]["results"]["channel"]["item"]["condition"]["temp"]
+desc = result["query"]["results"]["channel"]["item"]["condition"]["temp"]
+temp = result["query"]["results"]["channel"]["item"]["condition"]["temp"]
 
-    print "I'ts" + desc + "in {}, with a temperature of {} Farenheit".format(city.capitalize(), temp)
-    sys.stdout.flush()
+print "It's " + desc + " in {}, with a temperature of {} Farenheit.".format(city.capitalize(), temp)
+sys.stdout.flush()
