a2ec42398c1314d7398239635157c48cb839aaae
EricB <ericblancas23@gmail.com>
2018-09-11 14:57:26 -0700

    object detection finished

--- engine/boot.sh
+++ engine/boot.sh
@@ -1,14 +1,2 @@
-from tensorflow.contrib.keras.python.preprocessing import image
-from tensorflow.contrib.keras.python.keras.applications.inception_v3 import *
-import os
-import sys
-from flask import Flask, render_template, request
-import json
 
-app = Flask(__name__)
-
-model = InceptionV3(weights='imagnent')
-
-@app.route("/detect", methods=["GET", "POST"])
-def detect():
     
